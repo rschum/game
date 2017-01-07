d25e60eb73326e1de7b9288ad4b50b64a252383c
Master Foo <Master.Foo.v.1.0.0@gmail.com>
2017-01-06 17:13:29 -0700

    Update ./demo/build_workflow.py to work with logistics system

--- demo/build_workflow.py
+++ demo/build_workflow.py
@@ -7,41 +7,47 @@ from source.library.science.geology.rocks import bauxite
 from source.library.science.geology.rocks import ice
 from source.systems.logistics import logistics
 from source.concrete.entities.inanimate.rock import rock
+from source.abstract.location.model import model
+
+class BuildWorkflow(model.Model):
+    entities    = []
+    logistics   = None
+
+    def __init__(self):
+        model.Model.__init__(self)
+
+        self.logistics  = logistics.Logistics(self)
+        #Create some rocks
+        ice_rock = rock.Rock(self, 1.2, ice.Ice().composition)
+        bauxite_rock = rock.Rock(self, 0.95, bauxite.Bauxite().composition)
+        
+        self.entities = [
+            ice_rock,
+            bauxite_rock
+        ]
+
+    def print_composition_of_minerals(self):
+        for entity in self.entities:
+            entity.composition.pretty_print()
+
+    def put_minerals_in_the_refinery(self):
+        for entity in self.entities:
+            print("\033[94m"+"Refining rock and Storing it's Elements in the Elemental Storage Unit."+"\033[0m")
+            self.logistics.refinery.refine_mineral(entity)
+
+    def print_the_status_of_the_elemental_storage_unit(self):
+        self.logistics.elemental_storage_unit.pretty_print()
+
+    def replicate_a_widget(self):
+        widget0 = self.logistics.replicator.build_widget()
+        widget0.pretty_print()
 
 if __name__ == "__main__":
-    logistics  = logistics.Logistics()
-
-    #Create some rocks
-    ice_rock = rock.Rock(None, 1.2, ice.Ice().composition)
-    bauxite_rock = rock.Rock(None, 0.95, bauxite.Bauxite().composition)
-
-    #Print out the composition of the Minerals
-    ice_rock.composition.pretty_print()
-    bauxite_rock.composition.pretty_print()
-
-    #Put Minerals in the Refinery
-    print("\033[94m"+"Refining ice rock and Storing it's Elements in the Elemental Storage Unit."+"\033[0m")
-    logistics.refinery.refine_mineral(ice_rock)
-
-    print("\033[94m"+"Refining bauxite rock and Storing it's Elements in the Elemental Storage Unit."+"\033[0m")
-    logistics.refinery.refine_mineral(bauxite_rock)
-
-    #Print out the status of the ElementalStorageUnit
-    logistics.elemental_storage_unit.pretty_print()
-
-    #Replicate a Widget
-    widget0 = logistics.replicator.build_widget()
-    widget0.pretty_print()
-
-    #Print out the status of the ElementalStorageUnit To see if Elements have been Removed
-    logistics.elemental_storage_unit.pretty_print()
-
-    #Replicate Another Widget
-    widget1 = logistics.replicator.build_widget()
-    widget1.pretty_print()
-
-    #This Widget should fail
-    widget2 = logistics.replicator.build_widget()
-
-    #Print out the status of the ElementalStorageUnit To see if Elements have been Removed
-    logistics.elemental_storage_unit.pretty_print()
+    build_workflow = BuildWorkflow()
+    build_workflow.print_composition_of_minerals()
+    build_workflow.put_minerals_in_the_refinery()
+    build_workflow.print_the_status_of_the_elemental_storage_unit()
+    build_workflow.replicate_a_widget()
+    build_workflow.replicate_a_widget()
+    build_workflow.replicate_a_widget()
+    build_workflow.print_the_status_of_the_elemental_storage_unit()
