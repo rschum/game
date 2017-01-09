3a58d5a3bf133191fe31a6456267a791af9fedad
Master Foo <Master.Foo.v.1.0.0@gmail.com>
2017-01-08 22:48:09 -0700

    Move Corn on_render to Plant

--- source/abstract/entities/plant/view/view.py
+++ source/abstract/entities/plant/view/view.py
@@ -1,6 +1,20 @@
 from source.abstract.entities.inanimate.view import view
+from source.library.action import action
+
+from ..model import model
 
 class View(view.View):
+    healthy_seed       = None
+    healthy_sprout     = None
+    healthy_sapling    = None
+    healthy_mature     = None
+    healthy_ripe       = None
+    damaged_seed       = None
+    damaged_sprout     = None
+    damaged_sapling    = None
+    damaged_mature     = None
+    damaged_ripe       = None
+
     def __init__(self):
         view.View.__init__(self)
         self.dimensions.x = 32
@@ -8,4 +22,62 @@ class View(view.View):
 
         self.height = 32
         self.width = 32
+        pass
+
+    def on_render(self):
+        if self.health_state == model.HealthState.HEALTHY:
+            if self.created_delta() <= 10:
+                if self.growth_state != model.GrowthState.SEED:
+                    if self.healthy_seed is not None:
+                        self.growth_state = model.GrowthState.SEED
+                        self.animation = action.Action(self.healthy_seed.data)
+            elif self.created_delta() <= 20:
+                if self.growth_state != model.GrowthState.SPROUT:
+                    if self.healthy_sprout is not None:
+                        self.growth_state = model.GrowthState.SPROUT
+                        self.animation = action.Action(self.healthy_sprout.data)
+            elif self.created_delta() <= 30:
+                if self.growth_state != model.GrowthState.SAPLING:
+                    if self.healthy_sapling is not None:
+                        self.growth_state = model.GrowthState.SAPLING
+                        self.animation = action.Action(self.healthy_sapling.data)
+            elif self.created_delta() <= 40:
+                if self.growth_state != model.GrowthState.MATURE:
+                    if self.healthy_mature is not None:
+                        self.growth_state = model.GrowthState.MATURE
+                        self.animation = action.Action(self.healthy_mature.data)
+            elif self.created_delta() > 40:
+                if self.growth_state != model.GrowthState.RIPE:
+                    if self.healthy_ripe is not None:
+                        self.growth_state = model.GrowthState.RIPE
+                        self.animation = action.Action(self.healthy_ripe.data)
+        
+        if self.health_state == model.HealthState.DAMAGED:
+            if self.created_delta() <= 10:
+                if self.growth_state != model.GrowthState.SEED:
+                    if self.damaged_seed is not None:
+                        self.growth_state = model.GrowthState.SEED
+                        self.animation = action.Action(self.damaged_seed.data)
+            elif self.created_delta() <= 20:
+                if self.growth_state != model.GrowthState.SPROUT:
+                    if self.damaged_sprout is not None:
+                        self.growth_state = model.GrowthState.SPROUT
+                        self.animation = action.Action(self.damaged_sprout.data)
+            elif self.created_delta() <= 30:
+                if self.growth_state != model.GrowthState.SAPLING:
+                    if self.damaged_sapling is not None:
+                        self.growth_state = model.GrowthState.SAPLING
+                        self.animation = action.Action(self.damaged_sapling.data)
+            elif self.created_delta() <= 40:
+                if self.growth_state != model.GrowthState.MATURE:
+                    if self.damaged_mature is not None:
+                        self.growth_state = model.GrowthState.MATURE
+                        self.animation = action.Action(self.damaged_mature.data)
+            elif self.created_delta() > 40:
+                if self.growth_state != model.GrowthState.RIPE:
+                    if self.damaged_ripe is not None:
+                        self.growth_state = model.GrowthState.RIPE
+                        self.animation = action.Action(self.damaged_ripe.data)
+        
+        self.animation.on_render(self)
         pass
